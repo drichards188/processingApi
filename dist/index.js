3317b7b07ca24a6f2317df109f97a8f4c9fcfd13
drichards188 <github@cxoprotect.com>
2022-10-19 11:47:32 -0600

    jasmine init test is working

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.myFunc = void 0;
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3000;
+const myFunc = (num) => {
+    return num * num;
+};
+exports.myFunc = myFunc;
+app.get("/", (req, res) => {
+    res.send("Express + TypeScript Server");
+});
+app.listen(port, () => {
+    console.log(`Server is running at https://localhost:${port}`);
+});
