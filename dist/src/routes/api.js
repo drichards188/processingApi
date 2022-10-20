205e8e5654787472b0a19f25d7e7d358df939d4d
drichards188 <github@cxoprotect.com>
2022-10-20 11:13:11 -0600

    rendering image based on url

--- dist/src/routes/api.js
+++ dist/src/routes/api.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const routes = (0, express_1.Router)();
-const filepath = '/home/drich/assets/full/encenadaport.jpg';
 /* GET home page. */
 routes.get('/api', function (req, res, next) {
+    const filepath = '/home/drich/assets/full/' + req.query.id;
     res.sendFile(filepath);
     // res.send(`hiya david this is /api with params of: ${req.query.id}`)
     // res.render('index', { title: 'Hiya David this is /api' })
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -5,10 +5,9 @@ import { Router } from 'express'
 
 const routes = Router()
 
-const filepath = '/home/drich/assets/full/encenadaport.jpg'
-
 /* GET home page. */
 routes.get('/api', function (req: any, res: any, next: NextFunction) {
+    const filepath = '/home/drich/assets/full/' + req.query.id
     res.sendFile(filepath)
     // res.send(`hiya david this is /api with params of: ${req.query.id}`)
     // res.render('index', { title: 'Hiya David this is /api' })
